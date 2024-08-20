4f98668ea4566b8d86c28576ad4e656a3315be03
nukdokplex <nukdokplex@nukdokplex.ru>
2024-08-21 00:20:34 +0500

    removed default base16 scheme due to conflict

--- modules/homeManager/stylix.nix
+++ modules/homeManager/stylix.nix
@@ -3,7 +3,5 @@
 
   stylix = lib.mkDefault {
     enable = true;
-
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
   };
 }
--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -1,7 +1,5 @@
 { lib, pkgs, ... }: {
   stylix = lib.mkDefault {
     enable = true;
-
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
   };
 }
