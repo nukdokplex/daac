4892c37a8c4dffe6336e9b9190ddd864e3f9d227
nukdokplex <nukdokplex@nukdokplex.ru>
2024-08-20 23:41:04 +0500

    use tuigreet

--- instances/hosts/common/default.nix
+++ instances/hosts/common/default.nix
@@ -9,11 +9,10 @@
   programs.gamemode.enable = lib.mkDefault true;
   programs.hyprland.enable = lib.mkDefault true;
 
-  services.greetd.settings = rec {
-    initial_session = {
-      command = "${pkgs.hyprland}/bin/Hyprland";
-      user = "nukdokplex";
+  services.greetd.settings = {
+    default_session = {
+      command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd ${pkgs.hyprland}/bin/Hyprland";
+      user = "greeter";
     };
-    default_session = initial_session;
   };
 }
