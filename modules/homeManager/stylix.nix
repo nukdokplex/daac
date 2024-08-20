0394df69dc1486798ffc6d20599abb57ce3486c9
nukdokplex <nukdokplex@nukdokplex.ru>
2024-08-21 01:56:57 +0500

    replaced tokyo night with measured dark

--- modules/homeManager/stylix.nix
+++ modules/homeManager/stylix.nix
@@ -1,6 +1,6 @@
 { lib, pkgs, ... }: {
   stylix = {
     enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/measured-dark.yaml";
   };
 }
--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -2,7 +2,7 @@
   stylix = {
     enable = true;
     # https://www.pixel4k.com/wp-content/uploads/2018/09/tokyo-night-city-skyscrapers-4k_1538067528.jpg
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/measured-dark.yaml";
     image = pkgs.fetchurl {
       urls = [
         "https://www.pixel4k.com/wp-content/uploads/2018/09/tokyo-night-city-skyscrapers-4k_1538067528.jpg"
