55ec2e6a6b2fba8c4b9800b5d016e89ab306532a
nukdokplex <nukdokplex@nukdokplex.ru>
2024-08-20 20:00:18 +0500

    replaced spicetify-nix with worky fork

--- modules/homeManager/programs/spicetify.nix
+++ modules/homeManager/programs/spicetify.nix
@@ -1,13 +1,11 @@
 { self, pkgs, config, lib, ... }:
 let
-  spicetify-nix = self.inputs.spicetify-nix;
-  spicePkgs = spicetify-nix.packages.${pkgs.system}.default;
+  spicePkgs = self.inputs.spicetify-nix.legacyPackages.${pkgs.system};
 in
 {
-  imports = [ spicetify-nix.homeManagerModule ];
+  imports = [ self.inputs.spicetify-nix.homeManagerModules.default ];
 
   programs.spicetify = lib.mkDefault {
     enable = true;
-    spotifyPackage = pkgs.spotify;
   };
 }
