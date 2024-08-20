0f6cafec994ac5df04175a1140e3e0d6df8ce284
nukdokplex <nukdokplex@nukdokplex.ru>
2024-08-21 01:05:59 +0500

    added default wallpaper for stylix

--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -1,5 +1,15 @@
 { lib, pkgs, ... }: {
   stylix = lib.mkDefault {
     enable = true;
+    # https://www.pixel4k.com/wp-content/uploads/2018/09/tokyo-night-city-skyscrapers-4k_1538067528.jpg
+
+    image = pkgs.fetchurl {
+      urls = [
+        "https://www.pixel4k.com/wp-content/uploads/2018/09/tokyo-night-city-skyscrapers-4k_1538067528.jpg"
+        "https://web.archive.org/web/20240820194945/https://www.pixel4k.com/wp-content/uploads/2018/09/tokyo-night-city-skyscrapers-4k_1538067528.jpg"
+      ];
+
+      hash = "sha256-Ak/wZYbqmTxSq/U0e31HzAH5KZKqQJIKFEMLZCZNSC8=";
+    };
   };
 }
