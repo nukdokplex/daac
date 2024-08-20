6cccc589b5cad5c32274d0ae5c2515a1ca46c5f4
nukdokplex <nukdokplex@nukdokplex.ru>
2024-08-21 00:18:47 +0500

    added missing pkgs attribute in stylix modules

--- modules/homeManager/stylix.nix
+++ modules/homeManager/stylix.nix
@@ -1,4 +1,4 @@
-{ self, lib, ... }: {
+{ self, lib, pkgs, ... }: {
   imports = [ self.inputs.stylix.homeManagerModules.stylix ];
 
   stylix = lib.mkDefault {
--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -1,4 +1,4 @@
-{ lib, ... }: {
+{ lib, pkgs, ... }: {
   stylix = lib.mkDefault {
     enable = true;
 
