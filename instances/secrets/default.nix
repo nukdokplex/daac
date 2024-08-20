c7605a3ab2342ddb3a487022837cf88b7eda9839
nukdokplex <nukdokplex@nukdokplex.ru>
2024-08-21 04:44:23 +0500

    fixed secureboot secrets' wrong options

--- instances/secrets/default.nix
+++ instances/secrets/default.nix
@@ -2,14 +2,14 @@
   age.secrets = {
     # secureboot stuff
     sb_db_pem = {
-      file = "./secureboot/db/db.pem.age";
+      file = ./secureboot/db/db.pem.age;
       path = "/etc/secureboot/keys/db/db.pem";
       mode = "400";
       owner = "root";
       group = "root";
     };
     sb_db_key = {
-      file = "./secureboot/db/db.key.age";
+      file = ./secureboot/db/db.key.age;
       path = "/etc/secureboot/keys/db/db.key";
       mode = "400";
       owner = "root";
@@ -17,14 +17,14 @@
     };
 
     sb_KEK_pem = {
-      file = "./secureboot/KEK/KEK.pem.age";
+      file = ./secureboot/KEK/KEK.pem.age;
       path = "/etc/secureboot/keys/KEK/KEK.pem";
       mode = "400";
       owner = "root";
       group = "root";
     };
     sb_KEK_key = {
-      file = "./secureboot/KEK/KEK.key.age";
+      file = ./secureboot/KEK/KEK.key.age;
       path = "/etc/secureboot/keys/KEK/KEK.key";
       mode = "400";
       owner = "root";
@@ -32,14 +32,14 @@
     };
 
     sb_PK_pem = {
-      file = "./secureboot/PK/PK.pem.age";
+      file = ./secureboot/PK/PK.pem.age;
       path = "/etc/secureboot/keys/PK/PK.pem";
       mode = "400";
       owner = "root";
       group = "root";
     };
     sb_PK_key = {
-      file = "./secureboot/PK/PK.key.age";
+      file = ./secureboot/PK/PK.key.age;
       path = "/etc/secureboot/keys/PK/PK.key";
       mode = "400";
       owner = "root";
