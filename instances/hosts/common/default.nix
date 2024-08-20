c00f3a8bae7893e1503d47cce7713b26c8870953
nukdokplex <nukdokplex@nukdokplex.ru>
2024-08-20 20:36:29 +0500

    added greetd

--- instances/hosts/common/default.nix
+++ instances/hosts/common/default.nix
@@ -8,4 +8,12 @@
 
   programs.gamemode.enable = lib.mkDefault true;
   programs.hyprland.enable = lib.mkDefault true;
+
+  services.greetd.settings = rec {
+    initial_session = {
+      command = "${pkgs.hyprland}/bin/Hyprland";
+      user = "nukdokplex";
+    };
+    default_session = initial_session;
+  };
 }
--- modules/nixos/services/default.nix
+++ modules/nixos/services/default.nix
@@ -1,6 +1,7 @@
 { ... }: {
   imports = [
     ./fstrim.nix
+    ./greetd.nix
     ./gvfs.nix
     ./libinput.nix
     ./openssh.nix
