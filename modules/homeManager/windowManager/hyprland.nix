4d97325038c06329be04557af557acc32e95b306
nukdokplex <nukdokplex@nukdokplex.ru>
2024-08-22 02:45:36 +0500

    hyprland add media and brightness control binds

--- modules/homeManager/windowManager/hyprland.nix
+++ modules/homeManager/windowManager/hyprland.nix
@@ -143,6 +143,20 @@ in
         "$mainMod, mouse:273, resizewindow"
       ];
 
+      bindel = [
+        ", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+        ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+        ", XF86MonBrightnessDown, exec, brightnessctl set 10%-"
+        ", XF86MonBrightnessUp, exec, brightnessctl set 10%+"
+      ];
+
+      bindl = [
+        ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ", XF86AudioPlay, exec, playerctl play-pause"
+        ", XF86AudioPrev, exec, playerctl previous"
+        ", XF86AudioNext, exec, playerctl next"
+      ];
+
       # window rules
       windowrulev2 = [
         # xwaylandvideobridge
