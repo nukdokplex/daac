f322cd24e62bfe8e5189161d34d0d587f5f143e0
nukdokplex <nukdokplex@nukdokplex.ru>
2024-08-25 06:20:06 +0500

    remove piped redirects from forced to install firefox extensions

--- modules/nixos/programs/firefox.nix
+++ modules/nixos/programs/firefox.nix
@@ -46,10 +46,10 @@
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/firefox-color/latest.xpi";
           installation_mode = "force_installed";
         };
-        "{207f15ad-ca52-44b2-bc01-27330dbe6889}" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/piped-redirects/latest.xpi";
-          installation_mode = "force_installed";
-        };
+        # "{207f15ad-ca52-44b2-bc01-27330dbe6889}" = {
+        #   install_url = "https://addons.mozilla.org/firefox/downloads/latest/piped-redirects/latest.xpi";
+        #   installation_mode = "force_installed";
+        # };
         "{7a7a4a92-a2a0-41d1-9fd7-1e92480d612d}" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/styl-us/latest.xpi";
           installation_mode = "force_installed";
