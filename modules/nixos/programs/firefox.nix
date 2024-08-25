7696afaa895ed947d0ebd30452bdd61d7a8f7e17
nukdokplex <nukdokplex@nukdokplex.ru>
2024-08-25 06:17:12 +0500

    fixed typo in firefox policies definition

--- modules/nixos/programs/firefox.nix
+++ modules/nixos/programs/firefox.nix
@@ -37,7 +37,7 @@
         Locked = false;
       };
 
-      ExtensionsSettings = {
+      ExtensionSettings = {
         "uBlock0@raymondhill.net" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
           installation_mode = "force_installed";
