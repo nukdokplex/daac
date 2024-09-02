4e084699f727b81079b0a8e31f79b3b81a79042b
nukdokplex <nukdokplex@nukdokplex.ru>
2024-09-02 16:26:52 +0500

    rework gladr's partitioning scheme to make encrypted swap

--- instances/hosts/gladr/disko.nix
+++ instances/hosts/gladr/disko.nix
@@ -1,20 +1,22 @@
 {
   disko.devices = {
     disk = {
-      vdb = {
+      main = {
         type = "disk";
         device = "/dev/disk/by-id/nvme-HFM512GDJTNG-8310A_FY03N076611104A0B";
         content = {
           type = "gpt";
           partitions = {
             ESP = {
-              size = "1024M";
+              size = "1G";
               type = "EF00";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
-                mountOptions = [ "defaults" ];
+                mountOptions = [
+                  "defaults"
+                ];
               };
             };
             luks = {
@@ -23,12 +25,35 @@
                 type = "luks";
                 name = "crypted";
                 content = {
-                  type = "filesystem";
-                  format = "ext4";
-                  mountpoint = "/";
-                  mountOptions = [ "defaults" "noatime" ];
+                  type = "lvm_pv";
+                  vg = "main_pool";
                 };
               };
+            };
+          };
+        };
+      };
+    };
+    lvm_vg = {
+      main_pool = {
+        type = "lvm_vg";
+        lvs = {
+          swap = {
+            size = "16G";
+            content = {
+              type = "swap";
+              resumeDevice = true;
+            };
+          };
+          root = {
+            size = "100%FREE";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+              mountOptions = [
+                "defaults"
+              ];
             };
           };
         };
