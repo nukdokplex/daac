114ba3164c45d6ec421153dfeb8b78312e1dab9a
nukdokplex <nukdokplex@nukdokplex.ru>
2024-09-02 17:10:18 +0500

    moved nm profiles from gladr to common

--- instances/hosts/common/default.nix
+++ instances/hosts/common/default.nix
@@ -1,6 +1,7 @@
 { config, lib, pkgs, ... }: {
   imports = [
     ./users
+    ./networkManagerProfiles.nix
   ];
 
   hardware.enableRedistributableFirmware = true;
--- instances/hosts/gladr/default.nix
+++ instances/hosts/gladr/default.nix
@@ -51,49 +51,4 @@
       custom.usesBattery = true;
     }
   ];
-
-  networking.networkmanager.ensureProfiles = {
-    environmentFiles = [
-      config.age.secrets.wifi_passwords.path
-    ];
-
-    profiles = {
-      ndp-home-2 = {
-        connection = {
-          id = "ndp-home-2";
-          type = "wifi";
-        };
-
-        wifi = {
-          ssid = "ndp-home-2";
-          mode = "infrastructure";
-        };
-
-        wifi-security = {
-          key-mgmt = "wpa-psk";
-          psk = "$YGGDRASILS_PSK";
-        };
-        ipv4.method = "auto";
-        ipv6.method = "disabled";
-      };
-      ndp-home-5 = {
-        connection = {
-          id = "ndp-home-5";
-          type = "wifi";
-        };
-
-        wifi = {
-          ssid = "ndp-home-5";
-          mode = "infrastructure";
-        };
-
-        wifi-security = {
-          key-mgmt = "wpa-psk";
-          psk = "$YGGDRASILS_PSK";
-        };
-        ipv4.method = "auto";
-        ipv6.method = "disabled";
-      };
-    };
-  };
 }
