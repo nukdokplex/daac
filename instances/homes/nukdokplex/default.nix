a69e5dba959bbfa245f7e920c24de090e5819e08
nukdokplex <nukdokplex@nukdokplex.ru>
2024-09-04 19:49:23 +0500

    set xdg.userDirs for nukdokplex

--- instances/homes/nukdokplex/default.nix
+++ instances/homes/nukdokplex/default.nix
@@ -19,6 +19,18 @@ in
     ];
   };
 
+  xdg.userDirs = {
+    enable = true;
+    desktop = "${config.home.homeDirectory}/desktop";
+    documents = "${config.home.homeDirectory}/documents";
+    download = "${config.home.homeDirectory}/download";
+    music = "${config.home.homeDirectory}/music";
+    pictures = "${config.home.homeDirectory}/pictures";
+    publicShare = "${config.home.homeDirectory}/public_share";
+    templates = "${config.home.homeDirectory}/templates";
+    videos = "${config.home.homeDirectory}/videos";
+  };
+
   programs = {
     git = {
       userName = "nukdokplex";
@@ -40,4 +52,6 @@ in
       "$mainMod, E, exec, ${lib.getExe pkgs.pcmanfm}"
     ];
   };
+
+  
 }
