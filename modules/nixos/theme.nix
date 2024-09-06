f359624cbe5c4ee1b21e6fbae45af71455a0ae12
nukdokplex <nukdokplex@nukdokplex.ru>
2024-09-06 23:43:46 +0500

    added polarity to global stylix config

--- modules/nixos/theme.nix
+++ modules/nixos/theme.nix
@@ -3,6 +3,7 @@
     enable = true;
     # https://www.pixel4k.com/wp-content/uploads/2018/09/tokyo-night-city-skyscrapers-4k_1538067528.jpg
     base16Scheme = "${pkgs.base16-schemes}/share/themes/atelier-estuary.yaml";
+    polarity = "dark";
     image = pkgs.fetchurl {
       urls = [
         "https://www.pixel4k.com/wp-content/uploads/2018/09/tokyo-night-city-skyscrapers-4k_1538067528.jpg"
