19a51850b6de762c2c66c24f4e5504f4e6229ec3
nukdokplex <nukdokplex@nukdokplex.ru>
2024-09-08 02:31:40 +0500

    add vscodium configs for nix-ide

--- modules/homeManager/programs/vscodium.nix
+++ modules/homeManager/programs/vscodium.nix
@@ -1,4 +1,4 @@
-{ lib, config, pkgs, ... }: {
+{ self, lib, config, osConfig, pkgs, ... }: {
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
@@ -18,11 +18,19 @@
       # "terminal.integrated.minimumContrastRatio" = 1;
       "window.titleBarStyle" = "custom";
 
-      # catppuccin settings
       "window.commandCenter" = false;
       "window.menuBarVisibility" = "compact";
       "workbench.layoutControl.enabled" = false;
       "window.dialogStyle" = "custom";
+
+      # nix-ide settings
+      "nix.enableLanguageServer" = true;
+      "nix.serverPath" = lib.getExe pkgs.nixd;
+      "nix.serverSettings" = {
+        formatting = {
+          command = [ self.formatter.${osConfig.nixpkgs.hostPlatform.system} ];
+        };
+      };
     };
   };
 }
