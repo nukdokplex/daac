196caa999ef7fd27e4b47110d42974e3721bf728
nukdokplex <nukdokplex@nukdokplex.ru>
2024-11-07 22:52:56 +0500

    hypridle: add sleep for 2 seconds before suspend

--- modules/homeManager/windowManager/hyprland.nix
+++ modules/homeManager/windowManager/hyprland.nix
@@ -384,7 +384,7 @@ in
       general = {
         lock_cmd = "pidof swaylock || swaylock";
         unlock_cmd = "killall swaylock";
-        before_sleep_cmd = "loginctl lock-session"; # lock before suspend.
+        before_sleep_cmd = "loginctl lock-session && sleep 2"; # lock before suspend.
         after_sleep_cmd = "hyprctl dispatch dpms on"; # to avoid having to press a key twice to turn on the display.
       };
 
