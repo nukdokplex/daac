81fe808eaa1675613759098522db9588f6ee3d40
nukdokplex <nukdokplex@nukdokplex.ru>
2024-12-21 05:05:09 +0500

    gladr: configured sway's touchpad libinput

--- instances/hosts/gladr/default.nix
+++ instances/hosts/gladr/default.nix
@@ -70,8 +70,17 @@
       wayland.windowManager.hyprland.settings.monitor = [
         "desc:LG Display 0x05F6, 1920x1080, 0x0, 1.25"
       ];
-      wayland.windowManager.sway.config.output."LG Display 0x05F6 Unknown" = {
-        scale = "1.25";
+      wayland.windowManager.sway.config = {
+        output."LG Display 0x05F6 Unknown" = {
+          scale = "1.25";
+        };
+        input."1739:52545:SYN1B7F:00_06CB:CD41_Touchpad" = {
+          natural_scroll = "enabled";
+          dwt = "enabled";
+          tap = "enabled";
+          drag_lock = "enabled";
+          click_method = "button_areas";
+        };
       };
       custom.usesBattery = true;
       wayland.windowManager.sway.enable = true;
