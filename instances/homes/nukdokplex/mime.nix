a6723affc46151ff2c67b9f8dc9bea2964b7848c
nukdokplex <nukdokplex@nukdokplex.ru>
2024-12-21 06:13:31 +0500

    nukdokplex: configured mimeApps

--- instances/homes/nukdokplex/mime.nix
+++ instances/homes/nukdokplex/mime.nix
@@ -1,25 +1,21 @@
 { pkgs, lib, ... }:
 let open-tui = pkgs.writeShellScript "open-tui" (builtins.readFile ./scripts/open-tui.sh); in {
-  xdg.mime = {
+  xdg.mime.enable = true;
+  xdg.mimeApps = {
     enable = true;
-  };
+    defaultApplications = {
+      "text/plain" = "nvim.desktop";
+      "image/*" = "imv-folder.desktop";
+      "audio/*" = "mpv.desktop";
+      "video/*" = "mpv.desktop";
+      "inode/directory" = "xranger.desktop";
 
-  xdg.mimeApps.defaultApplications = {
-    "text/plain" = "nvim.desktop";
-    "image/*" = "imv-folder.desktop";
-    "audio/*" = "mpv.desktop";
-    "video/*" = "mpv.desktop";
-    "inode/directory" = "xranger.desktop";
-  };
-
-  xdg.desktopEntries.xranger = {
-    name = "xranger";
-    type = "Application";
-    terminal = false;
-    exec = "${lib.getExe open-tui} ranger %F";
-    icon = "utilities-terminal";
-    mimeType = [ "inode/directory" ];
-    categories = [ "System" "FileTools" "FileManager" ];
-    comment = "Launches the Ranger file manager";
+      "text/html" = [ "firefox.desktop" ];
+      "text/xml" = [ "firefox.desktop" ];
+      "application/xhtml+xml" = [ "firefox.desktop" ];
+      "application/vnd.mozilla.xul+xml" = [ "firefox.desktop" ];
+      "x-scheme-handler/http" = [ "firefox.desktop" ];
+      "x-scheme-handler/https" = [ "firefox.desktop" ];
+    };
   };
 }
