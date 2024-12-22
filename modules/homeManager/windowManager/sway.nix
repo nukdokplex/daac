db376aa9b1ca1c37f6b46fe3871abfb754c7958a
nukdokplex <nukdokplex@nukdokplex.ru>
2024-12-22 22:12:54 +0500

    sway: improved keybindigs

--- modules/homeManager/windowManager/sway.nix
+++ modules/homeManager/windowManager/sway.nix
@@ -14,7 +14,7 @@ in
       down = "j";
       terminal = "alacritty";
       menu = "'${lib.getExe pkgs.wofi}' --show drun";
-      defaultWorkspace = "1";
+      defaultWorkspace = "number 1";
 
       gaps.inner = 10;
       gaps.outer = 10;
@@ -54,6 +54,16 @@ in
         "${modifier}+Shift+Up" = "move up";
         "${modifier}+Shift+Right" = "move right";
 
+        "${modifier}+Ctrl+${left}" = "resize shrink width 10 px";
+        "${modifier}+Ctrl+${down}" = "resize grow height 10 px";
+        "${modifier}+Ctrl+${up}" = "resize shrink height 10 px";
+        "${modifier}+Ctrl+${right}" = "resize grow width 10 px";
+
+        "${modifier}+Ctrl+Left" = "resize shrink width 10 px";
+        "${modifier}+Ctrl+Down" = "resize grow height 10 px";
+        "${modifier}+Ctrl+Up" = "resize shrink heigt 10 px";
+        "${modifier}+Ctrl+Right" = "resize grow width 10 px";
+
         "${modifier}+b" = "splith";
         "${modifier}+v" = "splitv";
         "${modifier}+f" = "fullscreen toggle";
@@ -77,16 +87,16 @@ in
         "${modifier}+9" = "workspace number 9";
         "${modifier}+0" = "workspace number 10";
 
-        "${modifier}+Shift+1" = "move container to workspace number 1";
-        "${modifier}+Shift+2" = "move container to workspace number 2";
-        "${modifier}+Shift+3" = "move container to workspace number 3";
-        "${modifier}+Shift+4" = "move container to workspace number 4";
-        "${modifier}+Shift+5" = "move container to workspace number 5";
-        "${modifier}+Shift+6" = "move container to workspace number 6";
-        "${modifier}+Shift+7" = "move container to workspace number 7";
-        "${modifier}+Shift+8" = "move container to workspace number 8";
-        "${modifier}+Shift+9" = "move container to workspace number 9";
-        "${modifier}+Shift+0" = "move container to workspace number 10";
+        "${modifier}+Shift+1" = "move container to workspace number 1; workspace number 1";
+        "${modifier}+Shift+2" = "move container to workspace number 2; workspace number 2";
+        "${modifier}+Shift+3" = "move container to workspace number 3; workspace number 3";
+        "${modifier}+Shift+4" = "move container to workspace number 4; workspace number 4";
+        "${modifier}+Shift+5" = "move container to workspace number 5; workspace number 5";
+        "${modifier}+Shift+6" = "move container to workspace number 6; workspace number 6";
+        "${modifier}+Shift+7" = "move container to workspace number 7; workspace number 7";
+        "${modifier}+Shift+8" = "move container to workspace number 8; workspace number 8";
+        "${modifier}+Shift+9" = "move container to workspace number 9; workspace number 9";
+        "${modifier}+Shift+0" = "move container to workspace number 10; workspace number 10";
 
         "${modifier}+Shift+minus" = "move scratchpad";
         "${modifier}+minus" = "scratchpad show";
