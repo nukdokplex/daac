4a5321d246677782aa0d44f036da101312427d7a
nukdokplex <nukdokplex@nukdokplex.ru>
2025-01-08 16:40:56 +0500

    changed stylix image for common hosts

--- instances/hosts/common/stylix.nix
+++ instances/hosts/common/stylix.nix
@@ -4,15 +4,7 @@
     # https://www.pixel4k.com/wp-content/uploads/2018/09/tokyo-night-city-skyscrapers-4k_1538067528.jpg
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
     polarity = "dark";
-    image = pkgs.fetchurl {
-      urls = [
-        "https://www.pixel4k.com/wp-content/uploads/2018/09/tokyo-night-city-skyscrapers-4k_1538067528.jpg"
-        "https://web.archive.org/web/20240820194945/https://www.pixel4k.com/wp-content/uploads/2018/09/tokyo-night-city-skyscrapers-4k_1538067528.jpg"
-      ];
-
-      hash = "sha256-Ak/wZYbqmTxSq/U0e31HzAH5KZKqQJIKFEMLZCZNSC8=";
-    };
-
+    image = "${pkgs.nur.repos.nukdokplex.gruvbox-wallpapers.irl}/keyboards.jpg";
     cursor =
       {
         package = pkgs.adwaita-icon-theme;
