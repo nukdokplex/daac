c86340be88c5fd9abb018ab7ec3fa6d15e36441a
nukdokplex <nukdokplex@nukdokplex.ru>
2025-01-08 16:45:13 +0500

    nukdokplex: improved nixvim config

--- instances/homes/nukdokplex/nixvim/default.nix
+++ instances/homes/nukdokplex/nixvim/default.nix
@@ -1,5 +1,6 @@
 { pkgs, lib, ... }: {
   imports = [
-    ./plugins.nix
+    ./plugins
+    ./keymappings.nix
   ];
 }
---
+++ instances/homes/nukdokplex/nixvim/plugins/default.nix
@@ -0,0 +1,10 @@
+{
+  imports = [
+    ./neo-tree.nix
+    ./fzf-lua.nix
+    ./nix.nix
+    ./nvim-surround.nix
+    ./treesitter.nix
+    ./web-devicons.nix
+  ];
+}
