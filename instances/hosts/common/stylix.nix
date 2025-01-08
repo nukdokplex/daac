bcda90e8d7082500afe14ab46f34cbea0548cfb8
nukdokplex <nukdokplex@nukdokplex.ru>
2025-01-08 16:43:29 +0500

    another change of stylix image for common hosts

--- instances/hosts/common/stylix.nix
+++ instances/hosts/common/stylix.nix
@@ -4,7 +4,7 @@
     # https://www.pixel4k.com/wp-content/uploads/2018/09/tokyo-night-city-skyscrapers-4k_1538067528.jpg
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
     polarity = "dark";
-    image = "${pkgs.nur.repos.nukdokplex.gruvbox-wallpapers.irl}/keyboards.jpg";
+    image = "${pkgs.nur.repos.nukdokplex.gruvbox-wallpapers.irl}/board.png";
     cursor =
       {
         package = pkgs.adwaita-icon-theme;
