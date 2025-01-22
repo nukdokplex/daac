81221d767b61134afe5aa393ee78b253a4ff1e58
nukdokplex <nukdokplex@nukdokplex.ru>
2025-01-22 15:03:47 +0500

    sleipnir: add swtpm and ovmf to libvirtd configuration

--- instances/hosts/sleipnir/default.nix
+++ instances/hosts/sleipnir/default.nix
@@ -187,6 +187,19 @@
 
   virtualisation.libvirtd = {
     enable = true;
+    qemu = {
+      runAsRoot = true;
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [
+          (pkgs.OVMF.override {
+            secureBoot = true;
+            tpmSupport = true;
+          }).fd
+        ];
+      };
+    };
   };
 
   programs.virt-manager.enable = true;
